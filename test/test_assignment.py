37f9224035090bfbe56e70aafef23b61d321c79a
SherkeyXD <253294679@qq.com>
2024-06-20 19:13:56 +0800

    chore: pytest修正

--- test/test_assignment.py
+++ test/test_assignment.py
@@ -1,13 +1,14 @@
 import pytest
-import nonebot
+
 from nonebug import App
+from nonebot import logger
 
 
 @pytest.mark.asyncio
 async def test_assignment(app: App):
-    nonebot.load_from_toml("pyproject.toml")
     from nonebot_plugin_helldivers import Assignment
+
+    logger.debug("正在获取简报...")
     assignment = await Assignment.create()
     print(assignment)
-
-    
+    logger.debug("完成")
